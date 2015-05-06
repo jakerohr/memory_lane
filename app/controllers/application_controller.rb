4d575d118c696f00fcdf6b05317d9f925a4b0f8a
goopscoop <codybarrus@gmail.com>
2015-05-06 15:34:28 -0700

    Sessions are working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :current_user
+
+  def is_authenticated?
+    unless current_user
+      flash[:info] = "Oops. You need to log in to do that."
+      redirect_to login_path
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,24 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.authenticate(params[:user][:email],params[:user][:password])
+
+    if @user
+      session[:user_id] = @user.id
+      flash[:success] = "Login Successful"
+      redirect_to root_path
+    else
+      flash[:danger] = "Credentials Invalid"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:info] = "Thanks for strolling through Memory Ln. Come back soon."
+    redirect_to login_path
+  end
+
+
 end
