bf17858ce6cf98e8023c8cee9c79c45af784b56d
Jake Rohr <jakerohr@gmail.com>
2015-05-06 11:38:37 -0700

    testing github

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,13 @@
+class SiteController < ApplicationController
+  before_filter :find_model
+
+  def index
+
+  end
+
+
+  private
+  def find_model
+    @model = Site.find(params[:id]) if params[:id]
+  end
+end
---
+++ app/views/site/index.html.erb
@@ -0,0 +1,1 @@
+<h1>Test site</h1>
