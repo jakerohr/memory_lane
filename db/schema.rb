1f6ca6dbe3707394ba47793beac97f1e87f64b21
YOUR-USERNAME <aida_mengistu@hotmail.com>
2015-05-07 09:55:48 -0700

    updated collage partial

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150507011317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "pages", force: :cascade do |t|
     t.integer  "page_id"
@@ -62,7 +60,6 @@ ActiveRecord::Schema.define(version: 20150507011317) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "name"
   end
 
   add_foreign_key "pages", "users"
