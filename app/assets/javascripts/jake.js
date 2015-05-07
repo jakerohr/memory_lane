b7cd1db6211a65c0229e3d242733e1ae45d62be9
Jake Rohr <jakerohr@gmail.com>
2015-05-07 16:39:01 -0700

    added user_id folder on image upload

--- app/assets/javascripts/jake.js
+++ app/assets/javascripts/jake.js
@@ -2,10 +2,17 @@ $(function(){
   // set cloud name globally
   cloudinary.setCloudName('memoryln');
 
+  // pass in user_id
+  // gon gem
+  // gon.user_id (on controller)
+
   document.getElementById("upload_widget_opener").addEventListener("click", function() {
-    cloudinary.openUploadWidget({ cloud_name: 'memoryln', upload_preset: 'phzbi760', folder: 'test_images', tags: ['page1','partial1','current_user']},
+    cloudinary.openUploadWidget({ cloud_name: 'memoryln', upload_preset: 'phzbi760', folder: gon.current_user.id, tags: ['page1','partial1','current_user']},
     function(error, result) { console.log(error, result) });
   }, false);
+  // ajax post to the database
+  // this.scr = new result
+
 
   // cloudinary.applyUploadWidget(document.getElementById('opener'),
   //   { upload_preset: 'phzbi760', folder: 'test_images', tags: ['page1','partial1','current_user'] },
