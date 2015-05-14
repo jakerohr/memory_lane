5b486dad99d6e479930445084918206c5f3ac7fa
Jake Rohr <jakerohr@gmail.com>
2015-05-14 14:49:21 -0700

    story  partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,6 +31,8 @@ partial10 = Partial.create(path: "circle", name: "Circle", item1: "http://res.cl
 partial11 = Partial.create(path: "title_text_top", name: "Title Text Top", item1: "Your Title Here")
 partial12 = Partial.create(path: "title_subtitle", name: "Title & Subtitle", item1: "Your Title Here", item2: "...and you can add even more cool text here too!")
 partial13 = Partial.create(path: "welcome", name: "Welcome", item1: "Welcome", item2: "to", item3: "Memory", item4: "Lane", item5: "(scroll down)")
+partial14 = Partial.create(path: "story", name: "Story", item1: "Tell Your Story", item2: "through customizable", item3: "TEXT", item4: "http://res.cloudinary.com/memoryln/image/upload/l_text:Montserrat_150_bold:And Images!,co_rgb:020205,o_65/v1431036123/test_images/lighthouse-93487_1280.jpg")
+
 # Text overlay for image upload
 # l_text:Montserrat_90_bold:Click to Upload Photo,co_rgb:020205,o_65/
 
@@ -48,6 +50,7 @@ page.partials << partial10
 page.partials << partial11
 page.partials << partial12
 page.partials << partial13
+page.partials << partial14
 user.partials << partial1
 user.partials << partial2
 user.partials << partial3
@@ -61,6 +64,7 @@ user.partials << partial10
 user.partials << partial11
 user.partials << partial12
 user.partials << partial13
+user.partials << partial14
 
 p1 = PagesPartial.find_by_partial_id(1)
 p1.partial_order = 0
@@ -101,6 +105,9 @@ p10.save
 p13 = PagesPartial.find_by_partial_id(13)
 p13.partial_order = 12
 p13.save
+p14 = PagesPartial.find_by_partial_id(14)
+p14.partial_order = 13
+p14.save
 
 t1 = Background.create(bg_class: 'blue-gradiant', bg_name: 'Blue')
 t2 = Background.create(bg_class: 'teal-gradiant', bg_name: 'Teal')
@@ -127,10 +134,12 @@ t17.pages << page
 # HOMEPAGE PARTIALS
 
 home1 = Partial.create(path: "welcome", name: "Welcome", item1: "Welcome", item2: "to", item3: "Memory", item4: "Lane", item5: "(scroll down)")
-home2 = Partial.create(path: "title_text", name: "Title Text", item1: "Your Title Here")
+home2 = Partial.create(path: "story", name: "Story", item1: "Tell Your Story", item2: "through customizable", item3: "TEXT", item4: "http://res.cloudinary.com/memoryln/image/upload/l_text:Montserrat_150_bold:And Images!,co_rgb:020205,o_65/v1431036123/test_images/lighthouse-93487_1280.jpg")
 
 home_page.partials << home1
 home_page.partials << home2
+user.partials << home1
+user.partials << home2
 
 h1 = PagesPartial.find_by_partial_id(1)
 h1.partial_order = 0
